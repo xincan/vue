f06cf792115a16a3bf0c6b90847f6d0ce109323a
alittlexincan <alittlexincan@163.com>
2019-02-19 17:08:21 +0800

    添加table实列注释

--- vue-xincan/src/xincan/comments/Table.vue
+++ vue-xincan/src/xincan/comments/Table.vue
@@ -4,11 +4,8 @@
 
         <div class="hatech-search">
             <el-form :inline="true" :model="table.param" class="demo-form-inline">
-
                 <el-form-item><el-input v-model="table.param.loginName" placeholder="请输入登录名称"></el-input></el-form-item>
-
                 <el-form-item><el-input v-model="table.param.name" placeholder="请输入用户名称"></el-input></el-form-item>
-
                 <el-form-item>
                   <el-select v-model="table.param.sex" placeholder="请选择性别">
                     <el-option label="请选择性别" value=""></el-option>
@@ -16,20 +13,17 @@
                     <el-option label="女" value="0"></el-option>
                   </el-select>
                 </el-form-item>
-
                 <el-form-item><el-input v-model="table.param.areaId" placeholder="请出入用户所在地区"></el-input></el-form-item>
-
                 <el-form-item><el-input v-model="table.param.organizationId" placeholder="请输入用户所属机构"></el-input></el-form-item>
-
                 <el-form-item>
                     <el-button type="primary" size="small" @click="onTableSearch" icon="el-icon-search">查询</el-button>
                     <el-button type="warning" size="small" @click="onTableReset" icon="el-icon-delete">清空</el-button>
                 </el-form-item>
-
             </el-form>
         </div>
 
         <div class="hatech-table">
+            <!-- 表格头部布局 -->
             <div class="hatech-table-header">
                 <div class="hatech-table-header-left"><ul><li>{{table.title}}</li></ul></div>
                 <div class="hatech-table-header-right" v-if="table.showHeaderOption">
@@ -43,13 +37,14 @@
                               placement="bottom"
                               width="200"
                               trigger="click">
-                                <el-checkbox v-for="(column,key) in table.column" :key="key" :checked="column.isHide" :name="column.prop" @change="isShow(column)">{{column.label}}</el-checkbox>
+                                <el-checkbox v-for="(column,key) in table.column" :key="key" :checked="column.isHide" :name="column.prop" @change="onIsCellHide(column)">{{column.label}}</el-checkbox>
                                 <i class="el-icon-menu" slot="reference"></i>
                             </el-popover>
                         </li>
                     </ul>
                 </div>
             </div>
+            <!-- 表格主体布局 -->
             <div class="hatech-table-body">
                 <el-table
                   :data="table.data" stripe border size="small" resizable
@@ -67,12 +62,13 @@
                     <!-- 表格操作列设置 -->
                     <el-table-column prop="option" v-if="table.showTableOption" header-align="center" align="center" label="操作" fixed="right">
                         <template slot-scope="scope">
-                            <i  title="编辑" class="el-icon-edit-outline" @click="editUserOption(scope.$index, scope.row)"></i>
-                            <i  title="删除" class="el-icon-delete" @click="deleteUserOption(scope.$index, scope.row)"></i>
+                            <i  title="编辑" class="el-icon-edit-outline" @click="editTableRowOption(scope.$index, scope.row)"></i>
+                            <i  title="删除" class="el-icon-delete" @click="deleteTableRowOption(scope.$index, scope.row)"></i>
                         </template>
                     </el-table-column>
                 </el-table>
             </div>
+            <!-- 表格底部布局 -->
             <div class="hatech-table-footer">
                 <el-pagination
                   background
@@ -80,8 +76,7 @@
                   :current-page="table.page" :page-sizes="table.pageSize" :page-size="table.size" :total="table.count"
                   @size-change="tableSizeChange"
                   @current-change="tableCurrentChange"
-                >
-                </el-pagination>
+                ></el-pagination>
             </div>
         </div>
 
@@ -91,31 +86,31 @@
             <el-form :model="editForm">
 
                 <el-form-item label="登录名称" :label-width="editForm.formLabelWidth">
-                  <el-input v-model="editForm.data.loginName" autocomplete="off" placeholder="请输入登录名称" :style="'width: ' + editForm.formBqWidth"></el-input>
+                    <el-input v-model="editForm.data.loginName" autocomplete="off" placeholder="请输入登录名称" :style="'width: ' + editForm.formBqWidth"></el-input>
                 </el-form-item>
                 <el-form-item label="登录密码" :label-width="editForm.formLabelWidth">
-                  <el-input v-model="editForm.data.loginPassword" autocomplete="off" placeholder="请输入登录密码" :style="'width: ' + editForm.formBqWidth"></el-input>
+                    <el-input v-model="editForm.data.loginPassword" autocomplete="off" placeholder="请输入登录密码" :style="'width: ' + editForm.formBqWidth"></el-input>
                 </el-form-item>
                 <el-form-item label="用户名称" :label-width="editForm.formLabelWidth">
                     <el-input v-model="editForm.data.name" autocomplete="off" placeholder="请输入用户名称" :style="'width: ' + editForm.formBqWidth"></el-input>
                 </el-form-item>
                 <el-form-item label="用户性别" :label-width="editForm.formLabelWidth">
-                  <el-radio-group v-model="editForm.data.sex">
-                    <el-radio :label="0">女</el-radio>
-                    <el-radio :label="1">男</el-radio>
-                  </el-radio-group>
+                    <el-radio-group v-model="editForm.data.sex">
+                        <el-radio :label="0">女</el-radio>
+                        <el-radio :label="1">男</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="用户电话" :label-width="editForm.formLabelWidth">
-                  <el-input v-model="editForm.data.phone" autocomplete="off" placeholder="请输入用户电话" :style="'width: ' + editForm.formBqWidth"></el-input>
+                    <el-input v-model="editForm.data.phone" autocomplete="off" placeholder="请输入用户电话" :style="'width: ' + editForm.formBqWidth"></el-input>
                 </el-form-item>
                 <el-form-item label="用户邮箱" :label-width="editForm.formLabelWidth">
-                  <el-input v-model="editForm.data.email" autocomplete="off" placeholder="请输入用户邮箱" :style="'width: ' + editForm.formBqWidth"></el-input>
+                    <el-input v-model="editForm.data.email" autocomplete="off" placeholder="请输入用户邮箱" :style="'width: ' + editForm.formBqWidth"></el-input>
                 </el-form-item>
                 <el-form-item label="所属地区" :label-width="editForm.formLabelWidth">
-                  <el-input v-model="editForm.data.areaId" autocomplete="off" placeholder="请输入用户所属地区" :style="'width: ' + editForm.formBqWidth"></el-input>
+                    <el-input v-model="editForm.data.areaId" autocomplete="off" placeholder="请输入用户所属地区" :style="'width: ' + editForm.formBqWidth"></el-input>
                 </el-form-item>
                 <el-form-item label="所属机构" :label-width="editForm.formLabelWidth">
-                  <el-input v-model="editForm.data.organizationId" autocomplete="off" placeholder="请输入用户所属机构" :style="'width: ' + editForm.formBqWidth"></el-input>
+                    <el-input v-model="editForm.data.organizationId" autocomplete="off" placeholder="请输入用户所属机构" :style="'width: ' + editForm.formBqWidth"></el-input>
                 </el-form-item>
             </el-form>
             <div class="hatech-bottom-line"></div>
@@ -190,23 +185,29 @@
               ,{label:'所属机构', prop: 'organizationId', isHide: true}
               ,{label:'创建时间', prop: 'createTime',     isHide: true}
           ];
-          // 通过数据库查询用户保存的显隐列显示方式
-          this.getCellHide();
+          // 通过数据库查询用户保存的表格显隐列显示方式
+          this.initCellIsHide();
           // 初始化加载后台表格数据
           this.initTableData();
       }
 
       ,methods: {
 
-          // 初始化化读取数据库隐藏列
-          getCellHide(){
+          /**
+           * 初始化数据
+           * 初始化读取数据库隐藏列
+           * @Method initCellIsHide
+           */
+          initCellIsHide(){
               let that = this;
               Axios.get("http://127.0.0.1:3000/select/cell", {
                 params: {name:this.table.id}
               }).then( response => {
+                  // 将后台读取字符串JSON数据，转换成JSON数据
                   let cell = JSON.parse(response.data[0].content);
                   if(cell.length > 0){
                       let newColumn = [];
+                      // 比对两个集合修改列显示状态
                       that.table.column.forEach(clm => {
                           cell.forEach(cel => {
                               if(clm.prop === cel.prop){
@@ -215,21 +216,20 @@
                               }
                           });
                       });
-                      that.table.column=[];
-                      that.$nextTick(()=>{
+                      that.table.column=[];   // 清空列显示数据，后续紧跟着渲染使之vue执行树双向绑定
+                      that.$nextTick(()=>{    // 数据DOM元素重新渲染之前，加载数据
                           that.table.column = newColumn;
                       });
                   }
-
-              }).catch( error => {
-                  console.log(error);
-              });
+              }).catch( error => {console.log(error);});
           }
 
           /**
-           * @Method 显隐列操作
+           * 初始化数据
+           * 初始化加载列表数据
+           * @Method onIsCellHide
            */
-          ,isShow(column) {
+          ,onIsCellHide(column) {
               let cellString = '';
               this.table.column.forEach( cell => {
                   if( cell.prop === column.prop){
@@ -237,38 +237,34 @@
                   }
                   cellString += ',' + '{"prop":\"' + cell.prop + '\", "isHide\": ' + cell.isHide + '}';
               });
-
+              // 将操作的显隐列数据保存到后台
               Axios.get("http://127.0.0.1:3000/update/status", {
-                  params: {
-                      name:this.table.id
-                      ,content: "["+cellString.substr(1)+"]"
-                  }
-              }).then(function (response) {})
-                .catch(function (error) {});
+                  params: {name:this.table.id, content: "["+cellString.substr(1)+"]" }
+              }).then( response => {}).catch( error => {console.log(error);});
           }
 
-          // 初始化加载列表数据
-          ,initTableData(param){
+          /**
+           * 初始化数据
+           * 初始化加载列表数据
+           * @Method initTableData
+           */
+          ,initTableData(){
               let that = this;
               Axios.get("http://127.0.0.1:3000/user", {
-                  params: {
-                      page:this.table.page
-                      ,size:this.table.size
-                      ,param:this.table.param
-                  }
-              }).then(function (response) {
-                  that.$nextTick(()=>{
-                      let data = response.data;
-                      that.table.count = data.count;
-                      that.table.data = data.data;
+                  params: { page:this.table.page, size:this.table.size, param:this.table.param }
+              }).then( response => {
+                  that.$nextTick( ()=> {
+                      that.table.count = response.data.count;
+                      that.table.data = response.data.data;
                   });
 
-              }).catch(function (error) {});
+              }).catch(function (error) {console.log(error);});
           }
 
           /**
            * 表格分页
-           * @Method 每页条数
+           * 点击每页条数下拉列表进行选择数据进行查询
+           * @Method tableSizeChange
            */
           ,tableSizeChange(size) {
               this.table.size = size;
@@ -277,7 +273,9 @@
 
           /**
            * 表格分页
-           * @Method 当前页
+           * 首先数据选择渲染this.table.page数据
+           * 其次调用初始化table数据
+           * @Method 当前页选择操作
            */
           ,tableCurrentChange(page) {
               this.table.page = page;
@@ -285,40 +283,45 @@
           }
 
           /**
-           * 表格数据选中行
-           * @Method checkbox选中
+           * 表格操作
+           * 表格数据多选操作
+           * @Method tableChangeRows
            */
           ,tableChangeRows(row){
               this.table.select = row;
           }
 
           /**
-           * 表格自定义查询条件
-           * @Method 查询
+           * 头部表单操作
+           * 触发表单查询按钮，通过表单数据查询表格
+           * @Method onTableSearch
            */
           ,onTableSearch(){
               this.initTableData();
           }
 
           /**
-           * 表格自定义查询条件
-           * @Method 清空
+           * 头部表单操作
+           * 触发表单清空按钮，还原表单数据
+           * @Method onTableReset
            */
           ,onTableReset(){
               this.table.param = {loginName: '' ,name: '' ,sex: '' ,areaId: '' ,organizationId: ''};
           }
 
           /**
-           * 表格数据格式化
-           * @Method 性别格式化
+           * 表格列数据格式化
+           * 性别格式化
+           * @Method sexFormatter
            */
           ,sexFormatter(row, column, cellValue, index){
               return row.sex === 1 ? "男" : "女";
           }
 
           /**
-           * 表格导航栏
-           * @Method 添加按钮操作
+           * 表格头部操作
+           * 添加按钮操作，初始赋值，然后显示添加表单
+           * @Method onAddBar
            */
           ,onAddBar(){
               this.editForm.data = {sex:1};
@@ -326,8 +329,10 @@
           }
 
           /**
-           * 表格数据操作
-           * @Method 批量删除操作
+           * 表格头部操作
+           * 批量删除操作
+           * 判断表格是否有数据选中，如果没有选中，则显示友好提示信息，如果有选中项，则提示是否删除，否则取消删除
+           * @Method onDeleteBatchBar
            */
           ,onDeleteBatchBar() {
               let that = this
@@ -346,23 +351,29 @@
                 that.$message({message: response.data.msg ,center: true ,type: 'success'});
                 that.$nextTick(()=>{ that.initTableData(); });
               }).catch(function (error) {
-                that.$message({message: "数据操作失败" ,center: true ,type: 'fail'});
+                that.$message({message: "数据操作失败" ,center: true ,type: 'success'});
               });
 
           }
 
           /**
-           * @Method 修改
+           * 表格右侧列操作
+           * 编辑表格行数据信息
+           * 点击编辑按钮，进行选中数据双向绑定并显示编辑弹出层
+           * @Method editTableRowOption
            */
-          ,editUserOption(index, row) {
+          ,editTableRowOption(index, row) {
               this.editForm.data = row;
               this.editForm.dialogFormVisible = true;
           }
 
           /**
-           * @Method 删除
+           * 表格右侧列操作
+           * 删除表格行数据信息
+           * 点击删除按钮，根据选中数据ID进行删除
+           * @Method
            */
-          ,deleteUserOption(index, row) {
+          ,deleteTableRowOption(index, row) {
               let that = this;
               Axios.get("http://127.0.0.1:3000/user/delete", {
                 params: {id:row.id}
@@ -370,12 +381,14 @@
                   that.$message({message: response.data.msg ,center: true ,type: 'success'});
                   that.$nextTick(()=>{ that.initTableData(); });
               }).catch( error => {
-                  that.$message({message: "数据操作失败" ,center: true ,type: 'fail'});
+                  that.$message({message: "数据操作失败" ,center: true ,type: 'success'});
               });
           }
 
-          /** 弹出层
-           * @Method 弹出层提交
+          /**
+           * 弹出层操作
+           * 提交弹出层数据信息
+           * @Method editFormSubmit
            */
           ,editFormSubmit(){
               let that = this;
@@ -387,7 +400,7 @@
                   that.$message({message: response.data.msg ,center: true ,type: 'success'});
                   that.$nextTick(()=>{ that.initTableData(); });
               }).catch(function (error) {
-                  that.$message({message: "数据操作失败" ,center: true ,type: 'fail'});
+                  that.$message({message: "数据操作失败" ,center: true ,type: 'success'});
               });
           }
 
