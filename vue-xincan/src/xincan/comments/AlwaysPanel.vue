6c194e116a94cee67f1a27994ba0797b73330303
alittlexincan <alittlexincan@163.com>
2019-03-15 14:11:16 +0800

    添加布局方式

--- vue-xincan/src/xincan/comments/AlwaysPanel.vue
+++ vue-xincan/src/xincan/comments/AlwaysPanel.vue
@@ -4,22 +4,22 @@
 <template>
   <div class="hatech">
 
-    <div class="hatech-item hatech-cell-3">
-      <div style="background: red;height: 100%"></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-9">
       <div class="hatech-row">
-        <div class="hatech-item hatech-cell-12" style="background: red">
+
+        <div class="hatech-item hatech-cell-3 left"></div>
+
+        <div class="hatech-item hatech-cell-9 right">
+
+          <div class="hatech-row" style="height: 100%">
+
+            <div class="hatech-item hatech-cell-12 hatech-pmw" style="height:calc(20% - 5px);background: #62b4ff"></div>
+            <div class="hatech-item hatech-cell-12 hatech-pmw" style="height:calc(80% - 5px); margin-top:10px;background: #ffc05d"></div>
+
+          </div>
 
         </div>
-        <div class="hatech-item hatech-cell-12" style="background: blue">
 
-        </div>
       </div>
-
-    </div>
-
   </div>
 </template>
 
@@ -30,5 +30,12 @@
 </script>
 
 <style scoped lang="scss">
+  .left{
+    background: #1d21ff;
 
+  }
+
+  .right{
+    /*background: burlywood;*/
+  }
 </style>
--- vue-xincan/src/xincan/comments/Panel.vue
+++ vue-xincan/src/xincan/comments/Panel.vue
@@ -2,173 +2,202 @@
 <!--左右布局-->
 
 <template>
-  <div class="hatech">
+    <div class="hatech">
 
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-1">
-      <div></div>
-    </div>
+        <div class="hatech-row">
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+            <div class="hatech-item hatech-cell-1">
+              <div></div>
+            </div>
+        </div>
 
-    <div class="hatech-item hatech-cell-2">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-2">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-2">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-2">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-2">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-2">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-3">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-3">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-3">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-3">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-4">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-4">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-4">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-6">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-6">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-12">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-3">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-9">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-9">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-3">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-4">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-8">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-8">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-4">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-5">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-7">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-7">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-5">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-2">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-8">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-2">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-3">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-6">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-3">
-      <div></div>
-    </div>
-
-    <div class="hatech-item hatech-cell-4">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-4">
-      <div></div>
-    </div>
-    <div class="hatech-item hatech-cell-4">
-      <div></div>
-    </div>
+      <div class="hatech-row">
+          <div class="hatech-item hatech-cell-2">
+            <div></div>
+          </div>
+          <div class="hatech-item hatech-cell-2">
+            <div></div>
+          </div>
+          <div class="hatech-item hatech-cell-2">
+            <div></div>
+          </div>
+          <div class="hatech-item hatech-cell-2">
+            <div></div>
+          </div>
+          <div class="hatech-item hatech-cell-2">
+            <div></div>
+          </div>
+          <div class="hatech-item hatech-cell-2">
+            <div></div>
+          </div>
+      </div>
 
 
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-3">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-3">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-3">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-3">
+          <div></div>
+        </div>
+      </div>
 
-  </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-4">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-4">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-4">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-6">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-6">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-12">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-3">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-9">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-9">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-3">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-4">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-8">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-8">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-4">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-5">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-7">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-7">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-5">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-2">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-8">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-2">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-3">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-6">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-3">
+          <div></div>
+        </div>
+      </div>
+
+      <div class="hatech-row">
+        <div class="hatech-item hatech-cell-4">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-4">
+          <div></div>
+        </div>
+        <div class="hatech-item hatech-cell-4">
+          <div></div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
